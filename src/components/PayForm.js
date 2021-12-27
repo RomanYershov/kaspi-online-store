3c62a7b2694a8e69fb0df779e54e49cb70c8f9c2
рома ершов <energy26622662@outlook.com>
2021-12-27 13:14:17 +0600

    создание заказа в бд

--- src/components/PayForm.js
+++ src/components/PayForm.js
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 export default function PayForm(props){
+
+
+function orderInsert(){
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ address: address, cardNumber: cardNumber, state: 2 })}
+
+
+    fetch('https://localhost:5001/api/Order/OrderInsert', requestOptions)
+}
+
+const [address, setAddress] = useState("")
+const [cardNumber, setCardNumber] = useState("")
+
+
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    orderInsert()
+    alert('Заказ оплачен')
+  }
+
     return(
         <div className="block col-1">
             <h2>Форма оплаты</h2>
-            <div style={{paddingBottom: '15px'}}>
+           <form onSubmit={handleSubmit}>
+           <div style={{paddingBottom: '15px'}}>
                 Адрес:
-                <input title="test"></input>
+                <input value={address} onChange={(e) => setAddress(e.target.value)} title="test"></input>
             </div>
             <div>
                 Карта:
-                <input title="test"></input>
+                <input value={cardNumber} onChange={(e) => setCardNumber(e.target.value)} title="test"></input>
             </div>
+            <div>
+                <button type="submit">Оплатить</button>
+            </div>
+           </form>
         </div>
     )
 }
